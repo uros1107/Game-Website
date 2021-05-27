f46f995534d1416c3aa18f801ae32cd612c38300
amarchan9655 <amar.chan9655@gmail.com>
2021-05-28 02:47:16 +0900

    Updated

--- resources/views/frontend/ajax-m-monster-item.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="contain_shape contain_shape_{{ $monster->rarity }}">
-    <div class="shape"><img id="{{$monster->id}}"
-            src="{{ asset('images/game/icon_images/'.$monster->icon_image) }}"
-            alt="monster img"></div>
-    <input type="hidden" name="m_position[]" class="m_position" value="{{ $monster->id }}">
-</div>
-<span>{{ Session::get('lang') == 'en' ? $monster->name : $monster->fr_name }}</span>
